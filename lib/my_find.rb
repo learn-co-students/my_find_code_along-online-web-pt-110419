ce23d8657e818e28a142c20993ffe31e36c2e33c
Oksana Nichols <oksanahnichols@yahoo.com>
2019-11-18 07:06:40 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -6,5 +6,13 @@ escribe "#my_find" do
     expect(collection).to_not receive(:find)
   end
 def my_find(collection)
+require_relative 'spec_helper'
+require_relative '../lib/my_find.rb'
 
+describe "#my_find" do
+  let(:collection) { (1..100).to_a }
+
+  it "does not call on #find" do
+    expect(collection).to_not receive(:find)
+  end
 end
