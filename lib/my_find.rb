81e8e4e3aaaca808bfea1281c052bf0a9b7877b0
Oksana Nichols <oksanahnichols@yahoo.com>
2019-11-18 07:02:59 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,10 @@
 require 'pry'
+escribe "#my_find" do
+  let(:collection) { (1..100).to_a }
 
+  it "does not call on #find" do
+    expect(collection).to_not receive(:find)
+  end
 def my_find(collection)
 
 end
