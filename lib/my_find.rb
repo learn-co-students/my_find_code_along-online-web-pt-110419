1fefc81efc70e1b6d95c8a7fdd2bb235c40313e5
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-11-21 14:50:24 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,9 @@
-require 'pry'
-
-def my_find(collection)
-
+def my_find(1, 2, 3, 4, 5)
+i = 0 
+while i < collection.length yield(collection[i])
+  if yield(collection[i])
+    return collection[i]
+  end
+  i = i + 1 
+  end
 end
